f4a26801d924df8b672e104609b6ac0e1447f72a
Athos França <108290053+xxmelkezedek@users.noreply.github.com>
2024-10-22 23:01:40 -0300

    Pasta template/
    Pasta template/ adicionada

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_file
 from openai import OpenAI
 import os
 import markdown
 import bleach
+import datetime
 
 
 app = Flask(__name__)
@@ -24,11 +25,31 @@ client = OpenAI(
     api_key=api_key
 )
 
-mensagens = [{"role": "system", "content": "You are a helpful assistant."}]
+mensagens = [{"role": "system", "content": "You are a helpful Hacker assistant."}]
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/salvar_chat', methods=['POST'])
+def salvar_chat():
+    conteudo_chat = request.json['conteudo']
+
+    #cria diretorio chats se não existir
+    if not os.path.exists('chats'):
+        os.makedirs('chats')
+
+    #gera um nome de arquivo unico baseada na data e hora
+    nome_arquivo = f'chat_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.txt'
+    caminho_arquivo = os.path.join('chats', nome_arquivo)
+
+    #salva o conteudo do chat no arquivo criado a cima
+    with open(caminho_arquivo, 'w', encoding='utf-8') as arquivo:
+        arquivo.write(conteudo_chat)
+
+    #retorna o arquivo para download
+    return send_file(caminho_arquivo, as_attachment=True)
+
 
 @app.route('/chat', methods=['POST'])
 def chat():
